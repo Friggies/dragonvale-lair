b6a83792f77fd14fcbc950353f0a7470efac91e6
Friggies <85342562+Friggies@users.noreply.github.com>
2024-06-08 17:53:16 +0200

    added loading texts

--- src/utils/waitingTexts.js
+++ src/utils/waitingTexts.js
@@ -15,5 +15,9 @@ const waitingTexts = [
     'Check out the DragonVale Reddit community.',
     'Check out the DragonVale Forum community.',
     'Using data from The DragonVale Compendium.',
+    'Have you tried The DragonVale Sandbox?',
+    "The Blue Fire Dragon's breath is hotter!",
+    'Is it safe to hug the cute Blazing Dragon?',
+    'Checking for limited dragons...',
 ]
 export default waitingTexts
