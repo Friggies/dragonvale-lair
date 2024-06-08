87a90cd04de384d2c166d51979ad2ffd6f570095
Friggies <85342562+Friggies@users.noreply.github.com>
2024-06-08 12:24:09 +0200

    removed bahamut from selectable options

--- src/pages/breeding-simulator/index.js
+++ src/pages/breeding-simulator/index.js
@@ -88,6 +88,7 @@ export default function Home() {
 
     const options = dragons.map((dragon) => {
         if (dragon.name.includes(', the Legendary')) return {}
+        if (dragon.name.includes('Bahamut the Benevolent')) return {}
         return {
             value: dragon.name,
             label: dragon.name,
