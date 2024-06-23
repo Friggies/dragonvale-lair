f396be9a5f2b549404b6c12400a97f19da5203b4
Friggies <85342562+Friggies@users.noreply.github.com>
2024-06-23 19:44:47 +0200

    implemented in-memory locking

--- src/pages/api/breeding-simulator.js
+++ src/pages/api/breeding-simulator.js
@@ -1,4 +1,7 @@
 import { getBreedingData } from '@/utils/googleSheet'
+import Lock from '@/utils/lock'
+
+const lock = new Lock()
 
 export const config = {
     maxDuration: 60,
@@ -8,11 +11,14 @@ export default async function handler(req, res) {
     const { method } = req
 
     if (method === 'POST') {
+        await lock.acquire()
         try {
             const data = await getBreedingData(req.body)
             res.status(200).json(data)
         } catch (error) {
             res.status(500).json({ error: 'Failed to fetch data' })
+        } finally {
+            lock.release()
         }
     } else {
         res.setHeader('Allow', ['POST'])
---
+++ src/utils/lock.js
@@ -0,0 +1,23 @@
+export default class Lock {
+    constructor() {
+        this.locked = false
+        this.queue = []
+    }
+
+    async acquire() {
+        if (!this.locked) {
+            this.locked = true
+            return
+        }
+        return new Promise((resolve) => this.queue.push(resolve))
+    }
+
+    release() {
+        if (this.queue.length > 0) {
+            const next = this.queue.shift()
+            next()
+        } else {
+            this.locked = false
+        }
+    }
+}
