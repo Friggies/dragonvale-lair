b99122aad2c95c03ce709851af81335eba28bd42
Friggies <85342562+Friggies@users.noreply.github.com>
2024-06-23 12:44:48 +0200

    added new feature (quest matcher)

---
+++ src/pages/api/quest-matcher.js
@@ -0,0 +1,23 @@
+import dragons from '/public/dragons.json'
+
+export const config = {
+    maxDuration: 20,
+}
+
+export default async function handler(req, res) {
+    const { method } = req
+
+    if (method === 'POST') {
+        try {
+            const formData = JSON.parse(req.body)
+            const quest = formData.targetQuest
+            const dragon = dragons.find((dragon) => dragon.quest === quest)
+            res.status(200).json(dragon)
+        } catch (error) {
+            res.status(500).json({ error: 'Failed to fetch data' })
+        }
+    } else {
+        res.setHeader('Allow', ['POST'])
+        res.status(405).end(`Method ${method} Not Allowed`)
+    }
+}
