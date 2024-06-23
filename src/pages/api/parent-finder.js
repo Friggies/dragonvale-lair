81b1cffa2d9b03d7cdc8b22b219fd4e9c036039f
Friggies <85342562+Friggies@users.noreply.github.com>
2024-06-23 21:03:23 +0200

    removed production logs

--- src/pages/api/parent-finder.js
+++ src/pages/api/parent-finder.js
@@ -11,18 +11,14 @@ export default async function handler(req, res) {
     const { method } = req
 
     if (method === 'POST') {
-        console.log('Attempting to acquire lock...')
         await lock.acquire()
-        console.log('Lock acquired.')
         try {
             const data = await getParentData(req.body)
             res.status(200).json(data)
         } catch (error) {
             res.status(500).json({ error: 'Failed to fetch data' })
         } finally {
-            console.log('Releasing lock...')
             lock.release()
-            console.log('Lock released.')
         }
     } else {
         res.setHeader('Allow', ['POST'])
