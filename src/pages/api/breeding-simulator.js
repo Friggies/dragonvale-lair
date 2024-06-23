5745ea78e66c9d7e90d5b24a45eba31861d608da
Friggies <85342562+Friggies@users.noreply.github.com>
2024-06-23 20:51:00 +0200

    renamed lock and removed logs

--- src/pages/api/breeding-simulator.js
+++ src/pages/api/breeding-simulator.js
@@ -1,7 +1,7 @@
 import { getBreedingData } from '@/utils/googleSheet'
 import Lock from '@/utils/lock.js'
 
-const lock = new Lock()
+const breedingSimulatorLock = new Lock()
 
 export const config = {
     maxDuration: 60,
@@ -11,18 +11,14 @@ export default async function handler(req, res) {
     const { method } = req
 
     if (method === 'POST') {
-        console.log('Attempting to acquire lock...')
-        await lock.acquire()
-        console.log('Lock acquired.')
+        await breedingSimulatorLock.acquire()
         try {
             const data = await getBreedingData(req.body)
             res.status(200).json(data)
         } catch (error) {
             res.status(500).json({ error: 'Failed to fetch data' })
         } finally {
-            console.log('Releasing lock...')
-            lock.release()
-            console.log('Lock released.')
+            breedingSimulatorLock.release()
         }
     } else {
         res.setHeader('Allow', ['POST'])
