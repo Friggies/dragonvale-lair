8adce20ac4125ea0ace6dc3aefc628d0a576fb4f
Friggies <85342562+Friggies@users.noreply.github.com>
2024-06-23 21:03:38 +0200

    renamed the lock

--- src/pages/api/breeding-simulator.js
+++ src/pages/api/breeding-simulator.js
@@ -1,7 +1,7 @@
 import { getBreedingData } from '@/utils/googleSheet'
 import Lock from '@/utils/lock.js'
 
-const breedingSimulatorLock = new Lock()
+const lock = new Lock()
 
 export const config = {
     maxDuration: 60,
@@ -11,14 +11,14 @@ export default async function handler(req, res) {
     const { method } = req
 
     if (method === 'POST') {
-        await breedingSimulatorLock.acquire()
+        await lock.acquire()
         try {
             const data = await getBreedingData(req.body)
             res.status(200).json(data)
         } catch (error) {
             res.status(500).json({ error: 'Failed to fetch data' })
         } finally {
-            breedingSimulatorLock.release()
+            lock.release()
         }
     } else {
         res.setHeader('Allow', ['POST'])
