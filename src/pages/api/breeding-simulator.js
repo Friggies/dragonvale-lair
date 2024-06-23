fcf6714e4a95c016f46b9f3aa1147bfdce17ac2a
Friggies <85342562+Friggies@users.noreply.github.com>
2024-06-23 20:19:44 +0200

    added debugging logs for prod env

--- src/pages/api/breeding-simulator.js
+++ src/pages/api/breeding-simulator.js
@@ -1,5 +1,5 @@
 import { getBreedingData } from '@/utils/googleSheet'
-import Lock from '@/utils/lock'
+import Lock from '@/utils/lock.js'
 
 const lock = new Lock()
 
@@ -11,14 +11,18 @@ export default async function handler(req, res) {
     const { method } = req
 
     if (method === 'POST') {
+        console.log('Attempting to acquire lock...')
         await lock.acquire()
+        console.log('Lock acquired.')
         try {
             const data = await getBreedingData(req.body)
             res.status(200).json(data)
         } catch (error) {
             res.status(500).json({ error: 'Failed to fetch data' })
         } finally {
+            console.log('Releasing lock...')
             lock.release()
+            console.log('Lock released.')
         }
     } else {
         res.setHeader('Allow', ['POST'])
