8b71d7af8986c54eeba4e314a643673c86708b7f
Friggies <85342562+Friggies@users.noreply.github.com>
2024-06-27 18:37:24 +0200

    added statistics page

---
+++ src/utils/getMonthName.js
@@ -0,0 +1,17 @@
+export default function getMonthName(monthNumber) {
+    const monthNames = [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December',
+    ]
+    return monthNames[monthNumber - 1]
+}
