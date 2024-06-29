00b78c0060180f1e3e9628646ee6c38d59a13d25
Friggies <85342562+Friggies@users.noreply.github.com>
2024-06-29 12:27:59 +0200

    removed dragons that are unable to breed

--- src/pages/breeding-simulator/index.js
+++ src/pages/breeding-simulator/index.js
@@ -88,6 +88,8 @@ export default function Home() {
 
     const options = dragons.map((dragon) => {
         if (dragon.rarity.includes('Legendary')) return {}
+        if (dragon.rarity.includes('Gemstone')) return {}
+        if (dragon.elements.includes('Chrysalis')) return {}
         return {
             value: dragon.name,
             label: dragon.name,
