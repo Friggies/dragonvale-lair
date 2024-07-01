f5fe75bcca389d331076497cd87f5cbfdf693024
Friggies <85342562+Friggies@users.noreply.github.com>
2024-07-01 09:37:37 +0200

    simplified and corrected statistic counter

--- src/utils/addStatistic.js
+++ src/utils/addStatistic.js
@@ -23,51 +23,16 @@ function getCurrentYearAndMonth() {
 
 export default async function addStatistic(table) {
     const yearMonth = getCurrentYearAndMonth()
-    console.log(`Adding statistic for ${yearMonth} in table ${table}`)
 
     try {
-        const { data, error: selectError } = await supabase
-            .from(table)
-            .select('hits')
-            .eq('yearMonth', yearMonth)
-            .single()
-
-        if (selectError) {
-            console.log(selectError)
-            if (selectError.code === 'PGRST116') {
-                console.log(
-                    `No entry found for ${yearMonth}, inserting new record`
-                )
-                const { error: insertError } = await supabase
-                    .from(table)
-                    .insert([
-                        {
-                            yearMonth: yearMonth,
-                            hits: 1,
-                        },
-                    ])
-
-                if (insertError) {
-                    console.error(`Insert Error: ${insertError.message}`)
-                    throw new Error(`Could not create new ${table} statistic`)
-                }
-            } else {
-                console.error(`Select Error: ${selectError.message}`)
-                throw new Error(`Could not get hits from ${table} statistics`)
-            }
-        } else if (data) {
-            console.log(`Entry found for ${yearMonth}, updating hits`)
-            const { error: updateError } = await supabase
-                .from(table)
-                .update({ hits: data.hits + 1 })
-                .eq('yearMonth', yearMonth)
-
-            if (updateError) {
-                console.error(`Update Error: ${updateError.message}`)
-                throw new Error(`Could not add hit to ${table} statistics`)
-            }
+        const { error } = await supabase.rpc('incrementstatistic', {
+            yearmonth: yearMonth,
+            table_name: table,
+        })
+        if (error) {
+            throw new Error(error)
         }
     } catch (error) {
-        console.error(`Caught Error: ${error.message}`)
+        console.error(error)
     }
 }
