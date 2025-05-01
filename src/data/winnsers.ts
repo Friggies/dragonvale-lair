01cd2d067ef495c80baeb6e953d9770842e45430
Friggies <85342562+Friggies@users.noreply.github.com>
2025-05-01 18:28:34 +0200

    added old winners

--- src/data/winnsers.ts
+++ src/data/winnsers.ts
@@ -7,5 +7,25 @@ const winners = [
     'Usagi#6108',
     //test
     'DragonVale#1751522',
+    //minute madness
+    'Lumi 21167',
+    'adlerist#5274',
+    'MinyaZ 9443',
+    'Nori#5379',
+    'DragonVale#17535',
+    'burn#4705',
+    'kokorodaki#2613',
+    'AstroDell#0143',
+    'Culhacol#7767',
+    'Julz#2814',
+    'Kliesen#7875',
+    'Squish-E#8090',
+    //streak showdown
+    'Kliesen#7875',
+    'adlerist#5274',
+    'Citroosz#8010',
+    'Squish-E#8090',
+    'Usagi#6108',
+    'oromochi#4223',
 ]
 export default winners
