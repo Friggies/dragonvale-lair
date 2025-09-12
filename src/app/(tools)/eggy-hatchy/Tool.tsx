06e31983303096ad331d4f5019059033826df132
Frej Dahl Linneke <85342562+Friggies@users.noreply.github.com>
2025-09-12 06:53:52 +0200

    added disclaimer

--- src/app/(tools)/eggy-hatchy/Tool.tsx
+++ src/app/(tools)/eggy-hatchy/Tool.tsx
@@ -231,6 +231,10 @@ const Tool: React.FC = () => {
                         ? 'Generating board...'
                         : 'Begin'}
                 </button>
+                <p>
+                    The top five players will each receive their gem reward on
+                    September 23.
+                </p>
             </div>
         )
     }
