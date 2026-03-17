e2af802b83d9ee9b93eea2c731e19c52c9c49482
Frej Dahl Linneke <85342562+Friggies@users.noreply.github.com>
2026-03-17 20:18:16 +0100

    added chrysalis generator

--- src/app/(tools)/farming/Tool.tsx
+++ src/app/(tools)/farming/Tool.tsx
@@ -29,6 +29,7 @@ export default function Tool({
         Metal: 0,
         Dark: 0,
         Light: 0,
+        Chrysalis: 0,
     })
     // Reactive list of dragons (initially provided by page)
     const [dragonsList, setDragonsList] = useState<Dragon[]>(initialDragons)
